f4ee7acdc7d1b39db9e30e314fa8b07e53bc87c1
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-03-28 10:15:44 +0100

    Akan: updated

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Possibility from '../../assets/possibility.svg'
 import './possibility.css'
 
 export default function Possibility() {
   return (
-    <div className=' '>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={Possibility} alt="possibility" />
 
+      </div>
     </div>
   )
 }
