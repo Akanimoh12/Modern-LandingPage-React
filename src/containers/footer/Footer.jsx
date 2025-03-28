6ec0664528aa55c2c225ab31ef2036885a23beb1
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-03-29 00:27:13 +0100

    updated

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -5,9 +5,11 @@ import './footer.css'
 export default function Footer() {
   return (
     <div className='gpt3__footer section__padding'>
+
       <div className='gpt3__footer-heading'>
         <h1 className='gradient__text'>Do you want to step into the future before others</h1>
       </div>
+      
       <div className='gpt3__footer-btn'>
         <p>Request Early Access</p>
       </div>
